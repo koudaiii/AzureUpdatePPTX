3b44dfdf3815bf11610cb8a221a8f9b415103302
koudaiii <cs006061@gmail.com>
2025-01-16 10:34:25 +0900

    implements check env

--- main.py
+++ main.py
@@ -31,6 +31,29 @@ save_name = name_prefix + datetime.now().strftime('%Y%m%d') + str(random.randint
 
 # ボタンを押すと Azure Update API からデータを取得して PPTX を生成
 if st.button('PPTX 生成'):
+    # 環境変数の確認
+    st.info(
+        f"""
+    以下の環境変数が設定されています。 \n
+    API_KEY: {os.getenv("API_KEY")} \n
+    API_ENDPOINT: {os.getenv("API_ENDPOINT")} \n
+    API_VERSION: {os.getenv("API_VERSION")} \n
+    DEPLOYMENT_NAME: {os.getenv("DEPLOYMENT_NAME")} \n
+    AZURE_STORAGE_CONNECTION_STRING: {os.getenv("AZURE_STORAGE_CONNECTION_STRING")} \n
+    AZURE_STORAGE_ACCOUNT_CONTAINER_NAME: {os.getenv("AZURE_STORAGE_ACCOUNT_CONTAINER_NAME")} \n
+    """
+    )
+
+    # 環境変数が不足している場合はエラーを表示して終了
+    if (os.getenv("API_KEY") == ""
+        or os.getenv("API_ENDPOINT") == ""
+        or os.getenv("API_VERSION") == ""
+        or os.getenv("DEPLOYMENT_NAME") == ""
+        or os.getenv("AZURE_STORAGE_CONNECTION_STRING") == ""
+        or os.getenv("AZURE_STORAGE_ACCOUNT_CONTAINER_NAME") == ""):
+        st.error('環境変数が不足しています。.env ファイルを確認してください。 (Environment variables are missing. Please check the .env file.)')
+        st.stop()
+
     # Azure Update API からデータを取得
     st.write('データ取得中...')
     urls = azup.get_update_urls(days)
