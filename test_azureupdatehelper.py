cceb8c99a2585b06d803d0c8e387022e038b8b67
koudaiii <cs006061@gmail.com>
2025-01-24 11:57:40 +0900

    Add test case

--- test_azureupdatehelper.py
+++ test_azureupdatehelper.py
@@ -2,6 +2,8 @@ import unittest
 from unittest.mock import patch, MagicMock
 import azureupdatehelper
 import os
+from datetime import datetime
+import time
 
 
 class TestEnvironmentCheck(unittest.TestCase):
@@ -37,6 +39,60 @@ class TestGetRssFeedEntries(unittest.TestCase):
         entries = azureupdatehelper.get_rss_feed_entries()
         self.assertEqual(len(entries), 1)
         self.assertEqual(entries[0]['id'], '1')
+
+
+class TestGetUpdateUrls(unittest.TestCase):
+    @patch('azureupdatehelper.get_rss_feed_entries')
+    @patch('azureupdatehelper.datetime')
+    def test_get_update_urls_within_days(self, mock_datetime, mock_get_rss_feed_entries):
+        mock_datetime.now.return_value = datetime(2024, 11, 7)
+        mock_datetime.side_effect = lambda *args, **kw: datetime(*args, **kw)
+
+        mock_entry = MagicMock()
+        mock_entry.published = 'Sat, 2 Nov 2024 21:45:07 Z'
+        mock_entry.published_parsed = time.struct_time((2024, 11, 2, 21, 45, 7, 5, 307, 0))
+        mock_entry.link = 'https://example.com/update1'
+        mock_get_rss_feed_entries.return_value = [mock_entry]
+
+        mock_datetime.fromtimestamp.return_value = datetime(2024, 11, 2)
+
+        urls = azureupdatehelper.get_update_urls(7)
+        self.assertEqual(len(urls), 1)
+        self.assertEqual(urls[0], 'https://example.com/update1')
+
+    @patch('azureupdatehelper.get_rss_feed_entries')
+    @patch('azureupdatehelper.datetime')
+    def test_get_update_urls_outside_days(self, mock_datetime, mock_get_rss_feed_entries):
+        mock_datetime.now.return_value = datetime(2024, 11, 7)
+        mock_datetime.side_effect = lambda *args, **kw: datetime(*args, **kw)
+
+        mock_entry = MagicMock()
+        mock_entry.published = 'Sat, 2 Nov 2024 21:45:07 Z'
+        mock_entry.published_parsed = time.struct_time((2024, 11, 2, 21, 45, 7, 5, 307, 0))
+        mock_entry.link = 'https://example.com/update1'
+        mock_get_rss_feed_entries.return_value = [mock_entry]
+
+        mock_datetime.fromtimestamp.return_value = datetime(2024, 11, 2)
+
+        urls = azureupdatehelper.get_update_urls(1)
+        self.assertEqual(len(urls), 0)
+
+    @patch('azureupdatehelper.get_rss_feed_entries')
+    @patch('azureupdatehelper.datetime')
+    def test_get_update_urls_no_published_date(self, mock_datetime, mock_get_rss_feed_entries):
+        mock_datetime.now.return_value = datetime(2024, 11, 7)
+        mock_datetime.side_effect = lambda *args, **kw: datetime(*args, **kw)
+
+        mock_entry = MagicMock()
+        mock_entry.published = None
+        mock_entry.published_parsed = None
+        mock_entry.link = None
+        mock_get_rss_feed_entries.return_value = [mock_entry]
+
+        mock_datetime.fromtimestamp.return_value = None
+
+        urls = azureupdatehelper.get_update_urls(7)
+        self.assertEqual(len(urls), 0)
 
 
 if __name__ == '__main__':
